cf97ecbc3b76040946ff98479d03968efd8dea69
Jose Antonio <josegrobles1997@gmail.com>
2018-12-20 11:24:44 +0100

    Refactor Improve CLI readability

--- cli/cli.js
+++ cli/cli.js
@@ -10,57 +10,60 @@ let configFile = process.cwd();
 const params = process.argv.slice(2);
 
 if (params.length > 0) {
-  configFile = (fs.existsSync(params[0]) && fs.realpathSync(params.shift())) || process.cwd();
+	configFile = (fs.existsSync(params[0]) && fs.realpathSync(params.shift())) || process.cwd();
 }
-if (params.length > 1) return console.log('to many parameters')
-const tag = params.pop()
+if (params.length > 1) return console.log('to many parameters');
+const tag = params.pop();
 console.log(`consfigFile: ${configFile}, tag: ${tag}`);
-const config = require(configFile)
+const config = require(configFile);
 
-const topicFilter = _ => !tag || (tag && _ === tag)
 const createTopics = (topics, topicTemplate) => {
-  const topicNameList = Object.keys(topics)
-    .filter(topicFilter)
-    .map(_ => ({ name: _.replace('${LIBER_ENV}', envName), config: { ...topicTemplate, ...topics[_] } }))
+	const topicFilter = topic => !tag || (tag && topic === tag);
+	const topicNameList = Object.keys(topics)
+		.filter(topicFilter)
+		.map(topic => ({ name: topic.replace('${LIBER_ENV}', envName), config: { ...topicTemplate, ...topics[topic] } }));
 
-  const topicPromis = topicNameList.map(_ => ensureTopicExists(_.name, _.config, connectionInfo))
-  return Promise.all(topicPromis)
-    .then(async (data) => {
-      console.log('Success created topic(s)')
-    })
-    .catch((error) => {
-      console.error('Topic(s) create faild.')
-      console.error(error)
-    })
-}
+	const topicPromise = topicNameList.map(topic => ensureTopicExists(topic.name, topic.config, connectionInfo));
+	return Promise.all(topicPromise)
+		.then(() => {
+			console.log('Topic(s) creation was a success');
+		})
+		.catch(error => {
+			console.error('Topic(s) creation failed.');
+			console.error(error);
+		});
+};
 
 const createSubscription = (subscriptions, subscriptionTemplate) => {
-  const subscriptionNameList = [].concat(...Object.keys(subscriptions)
-    .filter(_ => !tag || (tag && subscriptions[_].topic !== tag))  // filter subscription by topic tag if tag exists
-    .map(_ => ({
-      name: _.replace('${LIBER_ENV}', envName),
-      topic: subscriptions[_].topic.replace('${LIBER_ENV}', envName),
-      config: { ...subscriptionTemplate, ...subscriptions[_], topic: undefined }
-    })) //prepare subscription obejct
-  )
-  return Promise.all(subscriptionNameList.map(_ => {return ensureSubscriptionExists(_.topic, _.name, _.config, connectionInfo)}))
-}
+	const subscriptionFilter = subscription => !tag || (tag && subscriptions[subscription].topic !== tag);
+	const subscriptionNameList = Object.keys(subscriptions || {})
+		.filter(subscriptionFilter) // filter subscription by topic tag if tag exists
+		.map(subscription => ({
+			name: subscription.replace('${LIBER_ENV}', envName),
+			topic: subscriptions[subscription].topic.replace('${LIBER_ENV}', envName),
+			config: { ...subscriptionTemplate, ...subscriptions[subscription], topic: undefined },
+		})); //prepare subscription obejct
+
+	return Promise.all(
+		subscriptionNameList.map(subscription => {
+			return ensureSubscriptionExists(subscription.topic, subscription.name, subscription.config, connectionInfo);
+		}),
+	);
+};
 
 const setupServiceBus = async () => {
-  try {
-    await createTopics(config.serviceBus.topics, config.serviceBus.topicCreate_Template);
-    console.log('Success setup Topics')
-    await createSubscription(config.serviceBus.subscriptions, config.serviceBus.subscriptionCreate_Template)
-    console.log('Success setup Subscriptions')
-  } catch (error) {
-    console.error('Faild setup Azure Service Bus', error)
-  }
-}
+	try {
+		await createTopics(config.serviceBus.topics, config.serviceBus.topicCreate_Template);
+		console.log('Success setting up Topics');
+		await createSubscription(config.serviceBus.subscriptions, config.serviceBus.subscriptionCreate_Template);
+		console.log('Success setting up Subscriptions');
+	} catch (error) {
+		console.error('Failed setting up Azure Service Bus', error);
+	}
+};
 
 if (config && config.serviceBus) {
-  setupServiceBus()
+	setupServiceBus();
+} else {
+	console.error('invalid config file');
 }
-else {
-  console.error('invalid config file')
-}
-
